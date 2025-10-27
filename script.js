0982dd48eec024a4f452fd39bbd3f7c55805fe0e
v-pun215 <vihaan.pundir26@gmail.com>
2025-10-27 22:52:07 +0530

    add basic layout + content

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    fixAge();
+};
+
+
+function fixAge() {
+    let age_span = document.getElementById("age");
+    const current_date = new Date();
+    const birthday = new Date("2011-03-15")
+    let age = current_date.getFullYear() - birthday.getFullYear()
+    if (current_date.getMonth() < birthday.getMonth() || (current_date.getMonth() === birthday.getMonth() && current_date.getDate() < birthday.getDate())) {
+        age--
+    }
+    age_span.innerHTML = age;
+}
